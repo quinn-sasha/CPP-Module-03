179993880fdbb1e31f0901b1b7f59ce04f0ecf48
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2026-01-08 20:38:49 +0900

    Added copy construcor and assignment operator

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -2,16 +2,34 @@
 #include <iostream>
 #include <string>
 
-ClapTrap::ClapTrap(const std::string &name)
+ClapTrap::ClapTrap(const std::string& name)
     : name(name), hit_points(10), energy_points(10), attack_damage(0) {
   std::cout << "CrapTrap " << name << " constructed" << std::endl;
+}
+
+ClapTrap::ClapTrap()
+    : name("unknown"), hit_points(10), energy_points(10), attack_damage(0) {}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+    : name(other.name), hit_points(other.hit_points),
+      energy_points(other.energy_points), attack_damage(other.attack_damage) {}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
+  if (this == &other) {
+    return *this;
+  }
+  name = other.name;
+  hit_points = other.hit_points;
+  energy_points = other.energy_points;
+  attack_damage = other.attack_damage;
+  return *this;
 }
 
 ClapTrap::~ClapTrap() {
   std::cout << "CrapTrap " << name << " destructed" << std::endl;
 }
 
-void ClapTrap::attack(const std::string &target) {
+void ClapTrap::attack(const std::string& target) {
   if (hit_points == 0 || energy_points == 0) {
     return;
   }
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -10,10 +10,13 @@ class ClapTrap {
   int attack_damage;
 
 public:
-  ClapTrap(const std::string &name);
+  ClapTrap();
+  ClapTrap(const std::string& name);
+  ClapTrap(const ClapTrap& other);
+  ClapTrap& operator=(const ClapTrap& other);
   ~ClapTrap();
 
-  void attack(const std::string &target);
+  void attack(const std::string& target);
   void takeDamage(unsigned int amount);
   void beRepaired(unsigned int amount);
   // For debug
